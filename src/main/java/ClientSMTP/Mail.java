fbddeee3b0b314fa932a3a2d9d0f5040b65b394d
Joshua Gomes da Costa <joshua.gomesdacosta@heig-vd.ch>
2019-04-08 11:52:40 +0200

    basic mail with to,from,cc,body

--- src/main/java/ClientSMTP/Mail.java
+++ src/main/java/ClientSMTP/Mail.java
@@ -1,5 +1,55 @@
 package ClientSMTP;
 
-public class Mail {
+import java.util.LinkedList;
 
+public class Mail {
+    private String from;
+    private LinkedList<String> to;
+    private LinkedList<String> cc;
+    private String body;
+
+    // Constructeurs ------------------------------------------------------------
+    public Mail(String from, LinkedList<String> to, LinkedList<String> cc, String body) {
+        this.from = from;
+        this.to = to;
+        this.cc = cc;
+        this.body = body;
+    }
+
+    public Mail() {
+    }
+
+    // Getters ------------------------------------------------------------
+    public String getFrom() {
+        return from;
+    }
+
+    public LinkedList<String> getTo() {
+        return to;
+    }
+
+    public LinkedList<String> getCc() {
+        return cc;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    // Setters ------------------------------------------------------------
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(LinkedList<String> to) {
+        this.to = to;
+    }
+
+    public void setCc(LinkedList<String> cc) {
+        this.cc = cc;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
