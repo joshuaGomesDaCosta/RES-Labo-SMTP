ace554fb1a7fba5b89ceaff7c529b240dccb7127
MICHAEL-DELL728\HEIG-Michael <michael.dasilva@heig-vd.ch>
2019-04-13 10:44:35 +0200

    Petit changement dans le fichier ReadPranksTest

--- src/test/java/applicationSMTP/clientSMTP/ReadPranksTest.java
+++ src/test/java/applicationSMTP/clientSMTP/ReadPranksTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ReadPranksTest {
     @Test
-    public void itShouldReadPranks(){
+    public void itShouldReadPranksFile(){
         String[] pranks = {"pranks1", "pranks2", "pranks3"};
-        String[] pranks2 = new String[0];
+        String[] pranks2;
 
         MailSender mailSender = MailSender.getInstance();
         mailSender.initialise("test", 2525, new File("C:/Users/dasil/RES_labo/SMTP/RES-Labo-SMTP/src/resources/MailPoolAdress.txt"), new File("C:/Users/dasil/RES_labo/SMTP/RES-Labo-SMTP/src/resources/Pranks.txt"));
