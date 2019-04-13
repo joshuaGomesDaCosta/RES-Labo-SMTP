be689f393fd283ea139d1df2b055aed064b22b76
MICHAEL-DELL728\HEIG-Michael <michael.dasilva@heig-vd.ch>
2019-04-13 13:41:52 +0200

    Chemin des fichiers fixed

--- src/main/java/applicationSMTP/Main.java
+++ src/main/java/applicationSMTP/Main.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Main {
     public void main(String[] args) {
         MailSender mailSender = MailSender.getInstance();
-        mailSender.initialise(new File("C:/Users/dasil/RES_labo/SMTP2/RES-Labo-SMTP/src/resources/Config.txt"), new File("C:/Users/dasil/RES_labo/SMTP2/RES-Labo-SMTP/src/resources/MailPoolAdress.txt"), new File("C:/Users/dasil/RES_labo/SMTP2/RES-Labo-SMTP/src/resources/Pranks.txt"));
+        mailSender.initialise(new File("./src/resources/Config.txt"), new File("./src/resources/MailPoolAdress.txt"), new File("./src/resources/Pranks.txt"));
 
         mailSender.send(1, 4);
         mailSender.send(3, 8);
